5f9e173ecd6fa295acc16eb7cad39d97de8a4b35
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-24 11:53:39 +0530

    Add files via upload

---
+++ Basic_Program/03 printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>;
+#include<conio.h>;
+
+int main()
+{
+    printf("\n\n\n\n Welcome To Fork Infosystem....");
+
+    getch();
+    return 0;
+}
+
---
+++ Basic_Program/04 If Programming.c
@@ -0,0 +1,17 @@
+#include<stdio.h>;
+#include<conio.h>;
+
+int main()
+{
+    int No = 0;
+
+    printf("Enter the value => ");
+    scanf("%d",&No);
+
+ if(No>0)
+    {
+      printf("\n Number is positive");
+    }
+    getch();
+    return 0;
+}
