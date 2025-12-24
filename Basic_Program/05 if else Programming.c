7bc84276f70dbefbe526576007117600acca3c38
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-24 11:57:12 +0530

    Add files via upload

---
+++ Basic_Program/05 if else Programming.c
@@ -0,0 +1,21 @@
+#include<stdio.h>;
+#include<conio.h>;
+
+int main()
+{
+    int No=0;
+
+    printf("\n Enter the value");
+    scanf("%d",&No);
+
+    if (No>0)
+        printf("\n number is positive");
+
+    else
+    {
+        printf("\n numbet is negative");
+
+    }
+    getch();
+    return 0;
+}
