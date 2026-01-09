575e6e8f06058b03141957896e87179022977749
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-01-09 14:33:32 +0530

    Add files via upload

---
+++ Array/Array With Intialization.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num[5]={10,20,30,40,50};
+    printf("\n Value Of Five Number");
+    printf("\n Value Of 1st Number = %d", Num[0]);
+    printf("\n Value Of 2nd Number = %d", Num[1]);
+    printf("\n Value Of 3rd Number = %d", Num[2]);
+    printf("\n Value Of 4th Number = %d", Num[3]);
+    printf("\n Value Of 5th Number = %d", Num[4]);
+
+    getch ();
+    return 0;
+
+}
