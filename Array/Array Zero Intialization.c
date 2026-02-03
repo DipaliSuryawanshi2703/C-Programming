4ea263a944196fdab49f1f5597fbaf3129786e7f
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-02-03 20:37:08 +0530

    Add files via upload

---
+++ Array/Array Zero Intialization.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num[5]={0};
+
+    printf("\n Value Of Five Number");
+
+    printf("\n Value Of 1st Number = %d", Num[0]);
+    printf("\n Value Of 2nd Number = %d", Num[1]);
+    printf("\n Value Of 3rd Number = %d", Num[2]);
+    printf("\n Value Of 4th Number = %d", Num[3]);
+    printf("\n Value Of 5th Number = %d", Num[4]);
+
+    getch ();
+    return 0;
+
+}
