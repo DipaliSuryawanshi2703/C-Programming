40a9ba8fda06c4bede77c68c48d55bcd6464d3fa
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-02-20 09:57:12 +0530

    Add files via upload

---
+++ Array Function/03 -7 Elements with Null (0) Initialization & Display All Elements.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void All_Element(int No[]);
+int main()
+{
+    int No[7]= {12,23,44,-67,890,672,78};
+
+    All_Element(No);
+
+    getch();
+    return 0;
+}
+void All_Element(int No[])
+{
+
+    printf("\n Enter Five Numbers ==> \n");
+
+    printf("\n First  Number is   %d ",No[0]);
+
+    printf("\n Second Number is   %d ",No[1]);
+
+    printf("\n Third  Number is   %d ",No[2]);
+
+    printf("\n Fourth Number is   %d ",No[3]);
+
+    printf("\n Fifth  Number is   %d ",No[4]);
+    ;
+    printf("\n Sixth  Number is   %d ",No[5]);
+
+    printf("\n Seventh Number is  %d ",No[6]);
+}
+
+
